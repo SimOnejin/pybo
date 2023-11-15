cdff1398fb0df076a44d28c39a7d423183a16ec0
심원진 <simwj0227@gmail.com>
2023-11-15 19:07:37 +0900

    데이터베이스 변경

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -11,10 +11,10 @@ environ.Env.read_env(BASE_DIR / '.env')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
+        'NAME': 'pybo',
+        'USER': 'dbmasteruser',
+        'PASSWORD': 'g6AgbzF6#grN#zhGmh!#2lz<4&z8%;ns',
+        'HOST': 'ls-928cdf55379927d357b9fee1be084643f786671e.cf5myg7wocco.ap-northeast-2.rds.amazonaws.com',
         'PORT': '5432',
     }
 }
