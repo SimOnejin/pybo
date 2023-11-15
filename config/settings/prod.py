f65ff96e8b72a83b720b0b4c7783d611f6f56806
심원진 <simwj0227@gmail.com>
2023-11-15 18:52:44 +0900

    데이터베이스 변경

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,5 +1,20 @@
 from .base import *
+import environ
 
 ALLOWED_HOSTS = ['43.202.220.99', 'onejin.link']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
+
+env = environ.Env()
+environ.Env.read_env(BASE_DIR / '.env')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': '5432',
+    }
+}
