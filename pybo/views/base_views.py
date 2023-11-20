c7d9288b6426f9e80f6207105ecc336089656d8b
심원진 <simwj0227@gmail.com>
2023-11-20 18:18:19 +0900

    단어 세션 적용

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -16,8 +16,9 @@ def index(request):
     """
     pybo 목록 출력
     """
-    request.session.modified = True  # 세션 삭제 가능하도록 등록
-    del request.session['texts']  # 세션 삭제
+    try:
+        request.session.modified = True  # 세션 삭제 가능하도록 등록
+        del request.session['texts']  # 세션 삭제
 
     # 입력 파라미터
     page = request.GET.get('page', '1')  # 페이지
