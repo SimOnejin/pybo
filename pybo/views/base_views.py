b7473fd790e1a997747b54e767b381ddf39ba933
심원진 <simwj0227@gmail.com>
2023-11-20 17:40:03 +0900

    단어 세션 적용

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -68,6 +68,7 @@ def detail(request, question_id):
 #     return render(request, 'pybo/ocr.html', context)
 
 def ocrTest(request, question_id):
+    request.session['texts'] = []
     global texts, new_image_path
     question = get_object_or_404(Question, pk=question_id)
     # image_url = question.image.url
