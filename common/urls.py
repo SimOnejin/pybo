08e1b2beadd463b8880ce9c7a9368ad6eba66181
심원진 <simwj0227@gmail.com>
2023-11-27 19:50:35 +0900

    단어리스트 데이터베이스

--- common/urls.py
+++ common/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='common/login.html'), name='login'), #내장기능 사용(?)
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', views.signup, name='signup'),
-    path("test", views.test, name="test"),
+    path("test/", views.test, name="test"),
 ]
