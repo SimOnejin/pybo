6b900f7140df2861c6a521cc621e4c994d1a5e25
심원진 <simwj0227@gmail.com>
2023-11-29 13:37:51 +0900

    단어 시험

--- pybo/urls.py
+++ pybo/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
          base_views.voca_detail, name='voca_detail'),
     path('ocrVoca/<int:voca_id>/',
          base_views.ocrTest1, name='ocrTest1'),
-    path('vocaTest', base_views.vocaTest, name="vocaTest"),
+    path('vocaTest', voca_views.vocaTest, name="vocaTest"),
 
 
     # question_views.py
--- pybo/views/voca_views.py
+++ pybo/views/voca_views.py
@@ -60,4 +60,56 @@ def voca_save(request):
 
 
 
+@login_required(login_url='common:login')
+def vocaTest(request):
+    user_id = request.user
+
+    voca = Voca
+    vocaList = voca.objects.raw("select * from " + str(user_id) + "_voca")
+    # vocaList = [{'voca_japan' : 'あ', 'voca_korea' : '아'},
+    #             {'voca_japan' : 'い', 'voca_korea' : '이'},
+    #             {'voca_japan' : 'う', 'voca_korea' : '우'},
+    #             {'voca_japan' : 'え', 'voca_korea' : '에'},
+    #             {'voca_japan' : 'お', 'voca_korea' : '오'}]
+    random.shuffle(vocaList)
+
+
+    # 정답과 오답 자리를 섞기위한 리스트생성
+    randomPosition = []
+    for i in range(len(vocaList)):
+        ranList = [0,1,2,3]
+        random.shuffle(ranList)
+        randomPosition.append(ranList)
+
+
+    # 정답과 함께 출력할 오답의 리스트 생성 - 0번 인덱스가 정답 1~3번 인덱스가 오답
+    randAnswer = []
+    for i in range(len(vocaList)):
+        answers = []
+        answers.append(i)
+        for j in range(1, 4):
+            while (True):
+                answer = random.randrange(len(vocaList))
+
+                dupChk = True
+                for k in range(0, j):
+                    if (answers[k] == answer):
+                        dupChk = False
+                        break
+
+                if (dupChk):
+                    answers.append(answer)
+                    break
+
+        for j in range(4):
+            answers[j] = vocaList[answers[j]]["voca_korea"]
+
+        randAnswer.append(answers)
+
+    # vocaList = 단어장, randomPosition = 정답 위치, randAnswer = 선택지
+    datas = {"vocaList": vocaList,
+             "randomPosition": randomPosition,
+             "randAnswer": randAnswer}
+
+    return render(request, 'pybo/voca_test.html', datas)
 
