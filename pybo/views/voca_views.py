2d6bd4c0d7537385d6f9506b5b3d88863260d219
심원진 <simwj0227@gmail.com>
2023-12-01 14:36:25 +0900

    단어 시험

--- pybo/views/voca_views.py
+++ pybo/views/voca_views.py
@@ -55,7 +55,7 @@ def voca_save(request):
 
 
 @login_required(login_url='common:login')
-def vocaTest(request):
+def vocaTest1(request):
     user_id = request.user
     voca_list = VocaList()
     vocaList = voca_list.select(user_id=str(user_id), voca_class='f')
@@ -112,15 +112,15 @@ def vocaTest(request):
 
 
 @login_required(login_url='common:login')
-def vocaTest1(request):
-    user_id = request.user
-    voca = VocaList
-    vocaList = VocaList.raw("select * from " + str(user_id) + "_voca where voca_class='f'")
-    # vocaList = [{'voca_japan': 'あ', 'voca_korea': '아'},
-    #             {'voca_japan': 'い', 'voca_korea': '이'},
-    #             {'voca_japan': 'う', 'voca_korea': '우'},
-    #             {'voca_japan': 'え', 'voca_korea': '에'},
-    #             {'voca_japan': 'お', 'voca_korea': '오'}]
+def vocaTest(request):
+    # user_id = request.user
+    # voca = VocaList
+    # vocaList = VocaList.raw("select * from " + str(user_id) + "_voca where voca_class='f'")
+    vocaList = [{'voca_japan': 'あ', 'voca_korea': '아'},
+                {'voca_japan': 'い', 'voca_korea': '이'},
+                {'voca_japan': 'う', 'voca_korea': '우'},
+                {'voca_japan': 'え', 'voca_korea': '에'},
+                {'voca_japan': 'お', 'voca_korea': '오'}]
 
     random.shuffle(vocaList)
 
