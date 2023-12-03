1e0c3fac1b12cc18f7b24c38ff9d3d9a12748f60
심원진 <simwj0227@gmail.com>
2023-12-04 07:09:00 +0900

    단어 시험

--- pybo/views/voca_views.py
+++ pybo/views/voca_views.py
@@ -108,7 +108,7 @@ def vocaTest1(request):
 
 
 def vocaTest(request):
-    vocaList = VocaList.objects.all()
+    vocaList = VocaList.objects.count()
     context = {"vocaList" : vocaList}
     return render(request, 'pybo/voca_read.html', context)
 
